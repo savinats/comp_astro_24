77dfd16bb46053cd7fafed91c29ebe73a370aaa7
savinats <savina.tsichli@gmail.com>
2024-12-19 13:35:38 +0100

    deleted some irrelevant files

--- transit.py
+++ transit.py
@@ -4,30 +4,28 @@ import matplotlib.pyplot as plt
 
 #exoplanet MASCARA-3 Ab
 
-#Define parameters oof exoplanet
 params = batman.TransitParams()       #object to store transit parameters
 params.t0 = 2457146.6                 #time of inferior conjunction
 params.per = 5.5514926                #orbital period
-params.rp = 0.1278                    #planet radius (in units of stellar radii) after converting it from jupiter radii 
+params.rp = 0.1278                    #planet radius (in units of stellar radii)
 params.a = 0.06971                    #semi-major axis (in units of stellar radii)
 params.inc = 89.16                    #orbital inclination (in degrees)
 params.ecc = 0.085                    #eccentricity
 params.w = 41.                        #longitude of periastron (in degrees)
+params.limb_dark = "quadratic"        #limb darkening model
+params.u = [0.215, 0.29]              #limb darkening coefficients [u1, u2, u3, u4]
 
-#Here i assume that the time of the first transit is the time of inferior conjuction
-#I converted the planet radius to stellar radii bc the encyclopedia had it in jupiter radii  
+t = np.linspace(-3, 4, 500)           #times at which to calculate light curve
+m = batman.TransitModel(params, t)    #initializes model
+flux = m.light_curve(params)          #calculates light curve
 
-t = np.linspace(2400000, params.t0, 2500000)  #times at which to calculate light curve
-m = batman.TransitModel(params, t)     #initializes model
+new_flux = m.light_curve(params)
 
-flux = m.light_curve(params)           #calculates light curve
-
-#plot the transit
 plt.figure(figsize=(10, 6))
 plt.plot(t, flux, label='Transit Light Curve')
 plt.xlabel('Time (days)')
 plt.ylabel('Relative Flux')
-plt.title('Transit Light Curve')
+plt.title('Transit Light Curve of Mascara-3 Ab')
 plt.legend()
 plt.grid()
 plt.show()
